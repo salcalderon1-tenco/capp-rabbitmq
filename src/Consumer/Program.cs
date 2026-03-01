25d5e18dd60cc3a2a378747930836ff4a099690e
Salvador Calderon Franco <scalderon@tenco.mx>
2026-03-01 12:16:35 -0600

    Created base projects and its configuration

---
+++ src/Consumer/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+ .AddJsonFile("appsettings.json")
+ .Build();
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ src/Producer/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+ .AddJsonFile("appsettings.json")
+ .Build();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
